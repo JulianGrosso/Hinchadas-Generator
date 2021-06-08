1a84a74b74d514e3c199536c7e9d4ec277739b39
Julian Grosso <81781648+JulianGrosso@users.noreply.github.com>
2021-06-08 00:42:22 -0300

    Style + Funcionalidades + AudioMix
    Reorganización de botones.
    Cambios en css.
    Cambio a play/stop en botones de reacciones.
    Autoplay en desktop con .onload.
    Cambios en mezcla de audio.

--- js/funciones.js
+++ js/funciones.js
@@ -1,31 +1,13 @@
-const audioHinchada01 = document.getElementById("audio-hinchada-01");
-const botonHinchada01 = document.getElementById("boton-hinchada-01");
-audioHinchada01.volume = 0.45;
-
-botonHinchada01.onclick = function () {
-	if (audioHinchada01.paused) {
-		audioHinchada01.play();
-	} else {
-		audioHinchada01.pause();
-	}
-};
-
-const audioHinchada02 = document.getElementById("audio-hinchada-02");
-const botonHinchada02 = document.getElementById("boton-hinchada-02");
-audioHinchada02.volume = 0.35;
-
-botonHinchada02.onclick = function () {
-	if (audioHinchada02.paused) {
-		audioHinchada02.play();
-	} else {
-		audioHinchada02.pause();
-	}
-};
-
 const botonCancha = document.getElementById("boton-cancha");
 const audioBase01 = document.getElementById("audio-base01");
 const audioBase02 = document.getElementById("audio-base02");
 const audioBase03 = document.getElementById("audio-base03");
+
+window.onload = function () {
+	audioBase01.play();
+	audioBase02.play();
+	audioBase03.play();
+};
 
 botonCancha.onclick = function () {
 	if (audioBase01.paused && audioBase02.paused && audioBase03.paused) {
@@ -39,6 +21,30 @@ botonCancha.onclick = function () {
 	}
 };
 
+const audioHinchada01 = document.getElementById("audio-hinchada-01");
+const botonHinchada01 = document.getElementById("boton-hinchada-01");
+audioHinchada01.volume = 0.55;
+
+botonHinchada01.onclick = function () {
+	if (audioHinchada01.paused) {
+		audioHinchada01.play();
+	} else {
+		audioHinchada01.pause();
+	}
+};
+
+const audioHinchada02 = document.getElementById("audio-hinchada-02");
+const botonHinchada02 = document.getElementById("boton-hinchada-02");
+audioHinchada02.volume = 0.45;
+
+botonHinchada02.onclick = function () {
+	if (audioHinchada02.paused) {
+		audioHinchada02.play();
+	} else {
+		audioHinchada02.pause();
+	}
+};
+
 const botonReaccion01 = document.getElementById("reaccion-1");
 const audioReaccion01 = document.getElementById("audio-gol");
 audioReaccion01.volume = 0.9;
@@ -48,42 +54,46 @@ botonReaccion01.onclick = function () {
 		audioReaccion01.play();
 	} else {
 		audioReaccion01.pause();
+		audioReaccion01.currentTime = 0;
 	}
 };
 
 const botonReaccion02 = document.getElementById("reaccion-2");
 const audioReaccion02 = document.getElementById("audio-uh");
-audioReaccion02.volume = 0.7;
+audioReaccion02.volume = 0.8;
 
 botonReaccion02.onclick = function () {
 	if (audioReaccion02.paused) {
 		audioReaccion02.play();
 	} else {
 		audioReaccion02.pause();
+		audioReaccion02.currentTime = 0;
 	}
 };
 
 const botonReaccion03 = document.getElementById("reaccion-3");
 const audioReaccion03 = document.getElementById("audio-eh");
-audioReaccion03.volume = 0.7;
+audioReaccion03.volume = 0.9;
 
 botonReaccion03.onclick = function () {
 	if (audioReaccion03.paused) {
 		audioReaccion03.play();
 	} else {
 		audioReaccion03.pause();
+		audioReaccion03.currentTime = 0;
 	}
 };
 
 const botonReaccion04 = document.getElementById("reaccion-4");
 const audioReaccion04 = document.getElementById("audio-silbidos");
-audioReaccion04.volume = 0.7;
+audioReaccion04.volume = 0.75;
 
 botonReaccion04.onclick = function () {
 	if (audioReaccion04.paused) {
 		audioReaccion04.play();
 	} else {
 		audioReaccion04.pause();
+		audioReaccion04.currentTime = 0;
 	}
 };
 
